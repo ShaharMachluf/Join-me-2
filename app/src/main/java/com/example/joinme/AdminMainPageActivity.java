c26c3f8231c8323897bf421bc5a2fd2bd258119c
ShaharMachluf <shahar6261@gmail.com>
2022-11-28 14:40:29 +0200

    added admin page

---
+++ app/src/main/java/com/example/joinme/AdminMainPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.joinme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminMainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_main_page);
+    }
+}
