c72138da487c749505f39b8b02ff132e44115f63
ShaharMachluf <shahar6261@gmail.com>
2022-11-29 17:06:05 +0200

    problems with firebase solved

--- app/src/main/java/com/example/joinme/FirebaseBaseModel.java
+++ app/src/main/java/com/example/joinme/FirebaseBaseModel.java
@@ -1,8 +1,11 @@
 package com.example.joinme;
 
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class FirebaseBaseModel {
     protected DatabaseReference myRef;
 
-    public FirebaseBaseModel(){myRef = FirebaseDatabase.get}
+    public FirebaseBaseModel(){myRef = FirebaseDatabase.getInstance().getReference();}
 }
