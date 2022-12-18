6f2e8a963b16993924fa97976db63c4152e3861b
ShaharMachluf <shahar6261@gmail.com>
2022-12-18 17:03:12 +0200

    ready for now

--- app/src/main/java/com/example/joinme/StatisticsActivity.java
+++ app/src/main/java/com/example/joinme/StatisticsActivity.java
@@ -1,11 +1,14 @@
 package com.example.joinme;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import com.example.joinme.databinding.ActivityFindGroupBinding;
@@ -16,8 +19,12 @@ import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.AggregateQuery;
 import com.google.firebase.firestore.AggregateQuerySnapshot;
@@ -25,6 +32,8 @@ import com.google.firebase.firestore.AggregateSource;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 //import com.jjoe64.graphview.GraphView;
 
 import org.eazegraph.lib.charts.PieChart;
@@ -33,6 +42,7 @@ import org.eazegraph.lib.models.PieModel;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class StatisticsActivity extends AppCompatActivity {
 
@@ -46,9 +56,11 @@ public class StatisticsActivity extends AppCompatActivity {
     PieChart pieChart;
     public ArrayList<BarEntry> barArrayList;
     BarChart barChart;
+    BarChart hBarChart;
+    ArrayList<BarEntry> hBarArrayList;
+    String[] hBarUsers;
     private final String[] categories = {"Minnian", "Football", "Basketball", "Group games", "Volunteer", "Hang out"};
     private final String[] colors = {"#FFBB86FC", "#00FFFF", "#0000FF", "#00FF00", "#800000", "#FFFF00"};
-//    GraphView graphView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,7 +73,9 @@ public class StatisticsActivity extends AppCompatActivity {
         pieChart = findViewById(R.id.piechart);
         barChart = findViewById(R.id.barChart);
         barArrayList = new ArrayList<>();
-//        graphView = findViewById(R.id.graphView);
+        hBarArrayList = new ArrayList<>();
+        hBarChart = findViewById(R.id.fragment_horizontalbarchart_chart);
+        hBarUsers = new String[3];
 
         try {
             createPieChart();
@@ -74,6 +88,8 @@ public class StatisticsActivity extends AppCompatActivity {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        createHorizontalBarChart();
 
         //handle click, logout
         binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
@@ -94,6 +110,7 @@ public class StatisticsActivity extends AppCompatActivity {
     }
 
     private void createPieChart() throws InterruptedException {
+        //send each category to countCategory
         for(int i=0; i < categories.length; ++i){
             countCategory(categories[i], colors[i]);
         }
@@ -104,6 +121,10 @@ public class StatisticsActivity extends AppCompatActivity {
     }
 
     private void countCategory(String category, String color){
+        /**
+         * for each category, count the number of groups from it and add to the pie chart
+         */
+        //extract from db
         CollectionReference collection = db.collection("groups");
         Query query = collection.whereEqualTo("title", category);
         AggregateQuery countQuery = query.count();
@@ -111,6 +132,7 @@ public class StatisticsActivity extends AppCompatActivity {
             if (task.isSuccessful()) {
                 AggregateQuerySnapshot snapshot = task.getResult();
                 Log.d(TAG, "Count: " + snapshot.getCount());
+                //add to pie chart
                 pieChart.addPieSlice(
                         new PieModel(
                                 category,
@@ -123,12 +145,14 @@ public class StatisticsActivity extends AppCompatActivity {
     }
 
     private void createBarChart() throws InterruptedException {
+        //send to recursive function that creates the bar chart
         getBarData(0, 0);
     }
 
     private void getBarData(int i, int j) throws InterruptedException {
-        if(i == categories.length){
+        if(i == categories.length){//stop condition
             Log.d(TAG, "bar Count: " + barArrayList.toString());
+            //create bar chart
             BarDataSet barDataSet = new BarDataSet(barArrayList, "Compare groups that happened and didn't happen");
             BarData barData = new BarData(barDataSet);
             barChart.setData(barData);
@@ -138,6 +162,7 @@ public class StatisticsActivity extends AppCompatActivity {
             barDataSet.setStackLabels(categories);
             barDataSet.setValueTextSize(10f);
 
+            //set column names
             XAxis xAxis = barChart.getXAxis();
             xAxis.setTextSize(9f);
             xAxis.setValueFormatter(new ValueFormatter() {
@@ -150,7 +175,8 @@ public class StatisticsActivity extends AppCompatActivity {
         }
         else{
             CollectionReference collection = db.collection("groups");
-            if(j % 2 == 0){
+            if(j % 2 == 0){//general
+                //extract from db
                 Query query = collection.whereEqualTo("title", categories[i]);
                 AggregateQuery countQuery = query.count();
                 countQuery.get(AggregateSource.SERVER)
@@ -158,6 +184,7 @@ public class StatisticsActivity extends AppCompatActivity {
                             if (task.isSuccessful()) {
                                 AggregateQuerySnapshot snapshot = task.getResult();
                                 Log.d(TAG, "bar Count: " + snapshot.getCount());
+                                //add to bar chart
                                 barArrayList.add(new BarEntry(i, snapshot.getCount()));
                                 try {
                                     getBarData(i, j+1);
@@ -168,7 +195,8 @@ public class StatisticsActivity extends AppCompatActivity {
                                 Log.d(TAG, "Count failed: ", task.getException());
                             }
                         });
-            }else{
+            }else{//happened
+                //extract from db
                 Query query = collection.whereEqualTo("title", categories[i]).whereEqualTo("is_happened", true);
                 AggregateQuery countQuery = query.count();
                 countQuery.get(AggregateSource.SERVER)
@@ -176,6 +204,7 @@ public class StatisticsActivity extends AppCompatActivity {
                             if (task.isSuccessful()) {
                                 AggregateQuerySnapshot snapshot = task.getResult();
                                 Log.d(TAG, "bar Count: " + snapshot.getCount());
+                                //add to bar chart
                                 barArrayList.add(new BarEntry(i, snapshot.getCount()));
                                 try {
                                     getBarData(i+1, j+1);
@@ -188,5 +217,50 @@ public class StatisticsActivity extends AppCompatActivity {
                         });
             }
         }
+    }
+
+    private void createHorizontalBarChart() {
+        /**
+         * takes the number of groups the top 3 users opened and present it in horizontal bar chart
+         */
+        //extract from db
+        db.collection("usersById").orderBy("my_groups", Query.Direction.DESCENDING).limit(3).get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            int i=0;
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                hBarUsers[i] = document.getString("name");
+                                //add to bar chart
+                                List<String> myGroups = (List<String>)document.get("my_groups");
+                                hBarArrayList.add(new BarEntry(i, myGroups.size()));
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                i++;
+                            }
+
+                            //create bar chart
+                            BarDataSet barDataSet = new BarDataSet(hBarArrayList, "Top 3 users by number of groups the opened");
+                            BarData barData = new BarData(barDataSet);
+                            barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+                            barDataSet.setValueTextColor(Color.BLACK);
+                            barDataSet.setValueTextSize(10f);
+
+                            //set column names
+                            XAxis xAxis = hBarChart.getXAxis();
+                            xAxis.setValueFormatter(new com.github.mikephil.charting.formatter.IndexAxisValueFormatter(hBarUsers));
+
+                            ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+                            dataSets.add(barDataSet);
+                            hBarChart.getDescription().setEnabled(true);
+
+                            hBarChart.setData(barData);
+                            hBarChart.invalidate();
+
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 }
