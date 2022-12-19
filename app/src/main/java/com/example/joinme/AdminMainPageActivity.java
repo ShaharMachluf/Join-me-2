5fc250e780946991184d81f3bbb10403d1f26802
ShaharMachluf <shahar6261@gmail.com>
2022-12-19 15:25:30 +0200

    ready for now

--- app/src/main/java/com/example/joinme/AdminMainPageActivity.java
+++ app/src/main/java/com/example/joinme/AdminMainPageActivity.java
@@ -52,7 +52,7 @@ public class AdminMainPageActivity extends AppCompatActivity {
             }
         });
 
-        binding.deleteUserBtn.setOnClickListener(new View.OnClickListener() {
+        binding.blockUserBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(AdminMainPageActivity.this, DeleteUserActivity.class));
